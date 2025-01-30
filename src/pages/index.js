a8fbf78df30c0719e1d5be3b6e45ab1b481edd9e
Snooopyman <armando.snoop@gmail.com>
2025-01-30 17:19:29 +0100

    fix size 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,7 @@
 export default function Home() {
     return (
         <div className="w-screen h-screen flex justify-center bg-gray-100">
-            <iframe
-                src="/hitbosss.pdf"
-                className="w-full h-full max-w-[100vw] border-none"
-                style={{ height: "100vh" }}
-            ></iframe>
+            <iframe src='/hitbosss.pdf' />
         </div>
     );
 }
