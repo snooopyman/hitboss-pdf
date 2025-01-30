bfe55448d38ebcc279549cb5616041dd641747f8
Snooopyman <armando.snoop@gmail.com>
2025-01-30 17:56:00 +0100

    swqs

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,7 @@ export default function Home() {
         <div className="w-full h-screen flex justify-center items-center bg-gray-100">
             <iframe
                 src="/hitbosss.pdf"
-                className="w-full h-full max-w-full max-h-full border-none"
-                style={{ overflow: "auto" }}
+                className="w-full h-full border-none overflow-auto"
             />
         </div>
     );
