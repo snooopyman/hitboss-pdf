5418ade7e9df4f2423c77bedd2c902d5690bf910
Snooopyman <armando.snoop@gmail.com>
2025-01-30 17:31:05 +0100

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 export default function Home() {
     return (
         <div className="w-screen h-screen flex justify-center bg-gray-100">
-            <embed
-                src="/hitbosss.pdf"
+            <object
+                data="/hitbosss.pdf"
                 type="application/pdf"
                 className="w-full h-full"
                 style={{ minHeight: "100vh" }}
-            />
+            >
+                <p>Tu navegador no soporta la visualización de PDFs. <a href="/hitbosss.pdf">Descargar PDF</a></p>
+            </object>
         </div>
     );
 }
